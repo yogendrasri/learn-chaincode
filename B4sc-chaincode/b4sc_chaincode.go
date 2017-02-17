0d3321a1793ea4a4af0087e0f48e5d7852858924
yogendrasri <yogendrasri@gmail.com>
2017-02-17 15:48:49 +0530

    Compiled my code

--- B4sc-chaincode/b4sc_chaincode.go
+++ B4sc-chaincode/b4sc_chaincode.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 
@@ -11,126 +10,12 @@ import (
 type SampleChaincode struct {
 }
 
-/************** Store Waybill Starts ************************/
-/**
-Expected Input is {WayBillID : "123456", "CreatedDate" : "2016-10-09"}
-***
-**/
-type WayBillIndex struct {
-	index []string
-}
-
 //custom data models
 type WayBill struct {
-	WayBillID        string   `json:"wayBillID"`
-	CreatedDate      string   `json:"createdDate"`
-	LastModifiedDate string   `json:"lastModifiedDate"`
-	Status           string   `json:"status"`
-	CreatedBy        string   `json:"createdBy"`
-	PendingWith      string   `json:"pendingWith"`
-	Palettes         []string `json:"palettes"`
-}
-
-type MasterWayBill struct {
-	WayBillID        string   `json:"wayBillID"`
-	CreatedDate      string   `json:"createdDate"`
-	LastModifiedDate string   `json:"lastModifiedDate"`
-	Status           string   `json:"status"`
-	CreatedBy        string   `json:"createdBy"`
-	PendingWith      string   `json:"pendingWith"`
-	Palettes         []string `json:"palettes"`
-}
-
-func CreateWayBill(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	fmt.Println("Entering CreateWayBill")
-
-	if len(args) < 1 {
-		fmt.Println("JSON Data missing")
-		return nil, errors.New("JSON Data missing")
-	}
-	wayBill := parseWayBill(args[0])
-
-	err := stub.PutState(wayBill.WayBillID, []byte(args[0]))
-	if err != nil {
-		fmt.Println("Could not save way bill to ledger", err)
-		return nil, err
-	}
-
-	nerr := addWayBillIdToIndex(stub, wayBill.WayBillID)
-	if nerr != nil {
-		fmt.Println("Could not save way bill to ledger", err)
-		return nil, nerr
-	}
-
-	fmt.Println("Successfully saved way bill")
-	return nil, nil
-
-}
-
-func CreateMasterWayBill(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	fmt.Println("Entering CreateWayBill")
-
-	if len(args) < 1 {
-		fmt.Println("JSON Data missing")
-		return nil, errors.New("JSON Data missing")
-	}
-	wayBill := parseWayBill(args[0])
-
-	err := stub.PutState(wayBill.WayBillID, []byte(args[0]))
-	if err != nil {
-		fmt.Println("Could not save way bill to ledger", err)
-		return nil, err
-	}
-
-	/*nerr := addWayBillIdToIndex(stub, wayBill.WayBillID)
-	if nerr != nil {
-		fmt.Println("Could not save way bill to ledger", err)
-		return nil, nerr
-	}*/
-
-	fmt.Println("Successfully saved master way bill")
-	return nil, nil
-
-}
-
-func addWayBillIdToIndex(stub shim.ChaincodeStubInterface, wayBillId string) error {
-	var wayBillIndex WayBillIndex
-	indexByte, err := stub.GetState("WAYBILL_INDEX")
-	if err != nil {
-		json.Unmarshal(indexByte, &wayBillIndex)
-		newWayBillIds := wayBillIndex.index
-		newWayBillIds = append(newWayBillIds, wayBillId)
-		wayBillIndex.index = newWayBillIds
-
-		wayBillIndexString, _ := json.Marshal(wayBillIndex)
-
-		nerr := stub.PutState("WAYBILL_INDEX", []byte(wayBillIndexString))
-		if nerr != nil {
-			fmt.Println("Could not save way bill to ledger", err)
-			return nerr
-		}
-	} else {
-		var tmpIndex []string
-		tmpIndex = append(tmpIndex, wayBillId)
-		wayBillIndex.index = tmpIndex
-
-		wayBillIndexString, _ := json.Marshal(wayBillIndex)
-
-		nerr := stub.PutState("WAYBILL_INDEX", []byte(wayBillIndexString))
-		if nerr != nil {
-			fmt.Println("Could not save way bill to ledger", err)
-			return nerr
-		}
-	}
-
-	return nil
-}
-
-func parseWayBill(jsondata string) WayBill {
-	res := WayBill{}
-	json.Unmarshal([]byte(jsondata), &res)
-	fmt.Println(res)
-	return res
+	ID               string `json:"id"`
+	AssetType        string `json:"assetType"`
+	LastModifiedDate string `json:"lastModifiedDate"`
+	Quantity         int    `json:"quantity"`
 }
 
 func GetWayBill(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
@@ -150,49 +35,56 @@ func GetWayBill(stub shim.ChaincodeStubInterface, args []string) ([]byte, error)
 	return bytes, nil
 }
 
-func GetMasterWayBill(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	fmt.Println("Entering GetWayBill")
+func CreateWayBill(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Println("Entering CreateWayBill")
 
-	if len(args) < 1 {
-		fmt.Println("Invalid number of arguments")
-		return nil, errors.New("Missing way bill ID")
+	if len(args) < 2 {
+		fmt.Println("Invalid number of args")
+		return nil, errors.New("Expected atleast two arguments for way bill creation")
 	}
 
-	var masterWayBillId = args[0]
-	bytes, err := stub.GetState(masterWayBillId)
+	var wayBillId = args[0]
+	var wayBillInput = args[1]
+
+	err := stub.PutState(wayBillId, []byte(wayBillInput))
 	if err != nil {
-		fmt.Println("Could not fetch loan master way bill with id "+masterWayBillId+" from ledger", err)
+		fmt.Println("Could not save way bill to ledger", err)
 		return nil, err
 	}
-	return bytes, nil
+
+	fmt.Println("Successfully saved way bill")
+	return nil, nil
+
 }
 
-/************** Waybill Ends ************************/
-
-// Init resets all the things
 func (t *SampleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("Inside INIT for test chaincode")
-	return nil, nil
-}
-
-func (t *SampleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if function == "CreateWayBill" {
-		return CreateWayBill(stub, args)
-	} else if function == "CreateMasterWayBill" {
-		return CreateMasterWayBill(stub, args)
-	} else {
-		return nil, errors.New("Invalid function name " + function)
-	}
 	return nil, nil
 }
 
 func (t *SampleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	if function == "GetWayBill" {
 		return GetWayBill(stub, args)
-	} else if function == "GetMasterWayBill" {
-		return GetMasterWayBill(stub, args)
 	}
+	return nil, nil
+}
 
+func GetCertAttribute(stub shim.ChaincodeStubInterface, attributeName string) (string, error) {
+	fmt.Println("Entering GetCertAttribute")
+	attr, err := stub.ReadCertAttribute(attributeName)
+	if err != nil {
+		return "", errors.New("Couldn't get attribute " + attributeName + ". Error: " + err.Error())
+	}
+	attrString := string(attr)
+	return attrString, nil
+}
+
+func (t *SampleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	if function == "CreateWayBill" {
+		return CreateWayBill(stub, args)
+	} else {
+		return nil, errors.New("Invalid function name " + function)
+	}
 	return nil, nil
 }
 
@@ -203,4 +95,18 @@ func main() {
 	} else {
 		fmt.Println("SampleChaincode successfully started")
 	}
+
+	// var str = `{"id":"wb1","assetType":"chip","lastModifiedDate":"21/09/2016 2:30pm","quantity":10}`
+	// stub := shim.NewMockStub("mock", new(SampleChaincode))
+
+	// args := []string{"wb1", str}
+	// _, err := stub.MockInvoke("123", "CreateWayBill", args)
+	// fmt.Println(err)
+
+	// bytes, err := stub.MockQuery("GetWayBill", []string{"wb1"})
+	// var wb WayBill
+	// err = json.Unmarshal(bytes, &wb)
+
+	// fmt.Println(wb)
+
 }
